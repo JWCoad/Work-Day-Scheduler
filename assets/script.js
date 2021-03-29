f74e669de1403351872d4f193f798b17a059e9ee
Jesse Coad <jessecoad@gmail.com>
2021-03-29 21:08:16 +1100

    Cleaned up code

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,25 @@
 $(document).ready(function () {
 
     //Set time
-    const currentDateEl = $("header #currentDay");
+    const currentDay = $("header #currentDay");
 
-
+    // click caller
     var calClick = {};
+    var refreshCheck = moment();
 
-
-    var hourRendered = moment();
-
-
-    function renderCalendar(today, calClick) {
+    // Create table 
+    function createSceduler(today, calClick) {
 
         let rowTime = moment(today).hour(9);
         const calendar = $("div.container");
         calendar.empty();
 
-
+        // Creat sufficent tables
         for (let i = 1; i < 10; i++) {
 
             const row = $("<div>").addClass("row");
 
-
+            // Color change based on time
             let classOfHour = "";
             if (today.isBefore(rowTime, "hour")) {
                 classOfHour = "future"
@@ -44,7 +42,7 @@ $(document).ready(function () {
             rowTime.add(1, "hour");
 
 
-            hourRendered = moment();
+            refreshCheck = moment();
         };
     };
 
@@ -52,45 +50,45 @@ $(document).ready(function () {
 
     function initCalendar() {
         const today = moment();
-        currentDateEl.text(today.format('LL'));
-        renderCalendar(today, calClick);
+        currentDay.text(today.format('LL'));
+        createSceduler(today, calClick);
     };
 
-
-    function loadCal() {
+    // Call saved content
+    function loadSave() {
         const storedCal = JSON.parse(localStorage.getItem("calClick"));
         if (storedCal) {
             calClick = storedCal;
         };
     };
 
-
-    loadCal();
+    // On load preform:
+    loadSave();
     initCalendar();
-    hourTracker();
+    changeColor();
 
 
-
-    function hourTracker() {
-        const checkHourInterval = setInterval(function () {
-            if (moment().isAfter(hourRendered, "minute")) {
+    // Refesh checker
+    function changeColor() {
+        const checkRealTime = setInterval(function () {
+            if (moment().isAfter(refreshCheck, "minute")) {
                 initCalendar();
             }
-        }, 60000);
+        }, 50000);
     };
 
 
-
-    function storeCal() {
+    // Save to local storage
+    function saveLocal() {
         localStorage.setItem("calClick", JSON.stringify(calClick));
     };
 
 
-
+    // Save functionality
     $(document).on("click", "button.saveBtn", function (event) {
         let calDesc = event.currentTarget.parentElement.children[1].value;
         calClick[event.currentTarget.id] = calDesc;
-        storeCal();
+        saveLocal();
     });
 
 });
